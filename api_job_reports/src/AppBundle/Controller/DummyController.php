594f074875f301931ef4ba265e6e19c062f64735
dafermon <davidfm55@gmail.com>
2018-02-05 21:50:05 +0100

    Initial project commit

---
+++ api_job_reports/src/AppBundle/Controller/DummyController.php
@@ -0,0 +1,25 @@
+<?php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\View\View;
+//DFM no he usado entity aun
+//use AppBundle\Entity\User;
+
+class DummyController extends FOSRestController
+{
+    /**
+    * @Rest\Get("/dummy")
+    */
+    public function getAction()
+    {	
+		$dummyOutput = "TestDummyOutput";
+		
+        return $dummyOutput;
+    }
+}
